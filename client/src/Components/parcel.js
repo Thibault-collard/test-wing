3fee0a6a3e063c11eaac729da5ca50d0616a0ecc
Thibobo78 <thibault.collard@gmail.com>
2020-09-03 17:27:29 +0200

    [FEATURES] Solve problems weight

--- client/src/Components/parcel.js
+++ client/src/Components/parcel.js
@@ -12,6 +12,8 @@ let tmp_quantity = 1;
 let tmp_weight = 0;
 let weight_available = 30;
 let items = [];
+let parcel_number = 1;
+let palette_number = 1;
 
 function findCorrespondingWeight(item_orders){
 	for(let h=0; h < itemsList.items.length;h++){
@@ -30,14 +32,21 @@ for(let i=0; i<ordersList.orders.length;i++){
 		for(let k=0; k < ordersList.orders[i].items[j].quantity;k++){
 			tmp_weight = findCorrespondingWeight(ordersList.orders[i].items[j].item_id)
 			//console.log(tmp_weight)
-			weight_available -= parseFloat(tmp_weight)	
+			
+			if( weight_available + parseFloat(tmp_weight) < 0 ){
+				weight_available -= tmp_weight;
+				console.log(weight_available)
+				console.log(parseFloat(tmp_weight))
+			}	
 			if(weight_available > parseFloat(tmp_weight)){
-				console.log(ordersList.orders[i].items[j].quantity)
-				console.log(tmp_quantity)
+				//console.log(ordersList.orders[i].items[j].quantity)
+				//console.log(tmp_quantity)
 				//console.log(weight_available)
+				weight_available -= parseFloat(tmp_weight)
 				if(tmp_quantity == ordersList.orders[i].items[j].quantity){
 					//console.log('bouclage en base de donnée')
 					items.push({item_id:ordersList.orders[i].items[j].item_id,quantity:tmp_quantity})
+					
 					//console.log(ordersList.orders[i].items[j].item_id)
 					tmp_quantity = 0;
 				}
@@ -47,11 +56,16 @@ for(let i=0; i<ordersList.orders.length;i++){
 				//console.log('bouclage en base de donnée')
 				items.push({item_id:ordersList.orders[i].items[j].item_id,quantity:tmp_quantity})
 				//console.log(ordersList.orders[i].items[j].item_id)
-				console.log(ordersList.orders[i].items[j].quantity)
-				console.log(tmp_quantity)
+				//console.log(ordersList.orders[i].items[j].quantity)
+				//console.log(tmp_quantity)
 				//console.log(weight_available)
 				//console.log('colis parcel')
-				parcel.parcel.push({items, weight:30 - weight_available })
+				parcel_number++
+				if(parcel_number == 17){
+					palette_number++
+					parcel_number = 0;
+				}
+				parcel.parcel.push({items, weight:30 - weight_available, palette_number:palette_number })
 				items = [];	
 				weight_available = 30;
 				if(tmp_quantity == ordersList.orders[i].items[j].quantity){
@@ -63,8 +77,6 @@ for(let i=0; i<ordersList.orders.length;i++){
 			}
 			
 		}
-		console.log(parcel)
-
 
 				// 
 				// 	//console.log(ordersList.orders[i].items[j].quantity)
@@ -87,11 +99,12 @@ for(let i=0; i<ordersList.orders.length;i++){
 				
 			
 		//
-		//console.log(parcel)
+		
 		//console.log(ordersList.orders[i].items[j].quantity)
 	}
+	
 }
-
+console.log(parcel)
 class Parcel extends Component {
 	
 	
