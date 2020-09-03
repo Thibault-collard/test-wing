4c4329439d473a2deb9404a91f3a27f87795b079
Thibobo78 <thibault.collard@gmail.com>
2020-09-03 23:23:34 +0200

    [FEATURES] Tracking_id is available

--- client/src/Components/parcel.js
+++ client/src/Components/parcel.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import '../App.css';
+import axios from "axios";
 
 let ordersList = require('../data/orders.json');
 let itemsList = require('../data/items.json');
@@ -14,6 +15,7 @@ let weight_available = 30;
 let items = [];
 let parcel_number = 1;
 let palette_number = 1;
+let tracking_id;
 
 function findCorrespondingWeight(item_orders){
 	for(let h=0; h < itemsList.items.length;h++){
@@ -25,86 +27,72 @@ function findCorrespondingWeight(item_orders){
 	}
 }
 
+
+async function getNumbers(){
+	return await axios.get("https://helloacm.com/api/random/?n=15")
+};
+	
+function attrTrackingID(){
+	for(let l=0;l<parcel.parcel.length;l++){
+		getNumbers().then(res => {
+			parcel.parcel[l].tracking_id = res.data
+		})
+	}
+}
+
 for(let i=0; i<ordersList.orders.length;i++){
 //Iterate through the list of orders
 	for(let j=0; j<ordersList.orders[i].items.length;j++){
 		//Iterate through the list of item in the list of orders
 		for(let k=0; k < ordersList.orders[i].items[j].quantity;k++){
 			tmp_weight = findCorrespondingWeight(ordersList.orders[i].items[j].item_id)
-			//console.log(tmp_weight)
+			
 			if(weight_available - tmp_weight > 0 ){
 				weight_available -= tmp_weight;
 			}
 				
-				
-			if(weight_available > parseFloat(tmp_weight)){
-				//console.log(ordersList.orders[i].items[j].quantity)
-				//console.log(tmp_quantity)
-				//console.log(weight_available)
+			if(weight_available > parseFloat(tmp_weight) && k != ordersList.orders[i].items[j].quantity-1){
 				if(tmp_quantity == ordersList.orders[i].items[j].quantity){
-					//console.log('bouclage en base de donnée')
 					items.push({item_id:ordersList.orders[i].items[j].item_id,quantity:tmp_quantity})
-					
-					//console.log(ordersList.orders[i].items[j].item_id)
 					tmp_quantity = 0;
 				}
 				tmp_quantity++
 				
 			}else{
-				//console.log('bouclage en base de donnée')
-				items.push({item_id:ordersList.orders[i].items[j].item_id,quantity:tmp_quantity})
-				//console.log(ordersList.orders[i].items[j].item_id)
-				//console.log(ordersList.orders[i].items[j].quantity)
-				//console.log(tmp_quantity)
-				//console.log(weight_available)
-				//console.log('colis parcel')
-				parcel_number++
-				if(parcel_number == 17){
-					palette_number++
-					parcel_number = 0;
-				}
-				parcel.parcel.push({items, weight:30 - weight_available, palette_number:palette_number })
-				items = [];	
-				weight_available = 30;
-				if(tmp_quantity == ordersList.orders[i].items[j].quantity){
-					tmp_quantity = 1;
-				}
-				
-				//console.log(tmp_quantity)
-				//La tmp quantity continue tandis que le scan passe au produit suivant
-			}
-			
+					if(j == ordersList.orders[i].items.length-1 && k+1 != ordersList.orders[i].items[j].quantity-1 && ordersList.orders[i].items[j].quantity-tmp_quantity != 0){
+						items.push({item_id:ordersList.orders[i].items[j].item_id,quantity:ordersList.orders[i].items[j].quantity-tmp_quantity})
+					}else{
+						items.push({item_id:ordersList.orders[i].items[j].item_id,quantity:tmp_quantity})
+					}
+					parcel_number++
+					if(parcel_number == 17){
+						palette_number++
+						parcel_number = 0;
+					}
+					parcel.parcel.push({order_id:ordersList.orders[i].id,items, weight:30 - weight_available, tracking_id:"", palette_number:palette_number });
+					items = [];	
+					weight_available = 30;
+					if(tmp_quantity == ordersList.orders[i].items[j].quantity){
+						tmp_quantity = 1;
+					}
+			}	
 		}
-		tmp_quantity = 1;
-				// 
-				// 	//console.log(ordersList.orders[i].items[j].quantity)
-					
-				// 	if(weight_available > parseFloat(itemsList.items[h].weight)){
-				// 		//
-				// 		tmp_quantity++
-				// 		weight_available -= parseFloat(itemsList.items[h].weight)	
-				// 	}else{
-				// 		shopping_cart.push({items:[{item_id:ordersList.orders[i].items[j].item_id,quantity:tmp_quantity}]})
-				// 		tmp_quantity = 0;
-				// 		weight_available = 30;
-				// 	}
-				// 	console.log(shopping_cart)
-				// 	//console.log(itemsList.items[h].weight*ordersList.orders[i].items[j].quantity)
-				// }
-				
-				// tmp_quantity = 0;
-				
-				
-			
-		//
-		
-		//console.log(ordersList.orders[i].items[j].quantity)
+		tmp_quantity = 1;	
 	}
-	
 }
+attrTrackingID()
+
+
 console.log(parcel)
+
+
 class Parcel extends Component {
-	
+	constructor() {
+    super();
+    this.state = {
+      items: []
+    };
+  }
 	
   render() {
     return (
